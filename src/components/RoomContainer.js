44d4960da077bb7a565f2d7acd699531f0228d21
umit uysal <umituysal48@hotmail.com>
2020-01-07 13:37:00 +0300

    basit otel projesi

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import RoomList from "./RoomList"
+import RoomsFilter from "./RoomsFilter"
+import { withRoomConsumer } from "../context"
+import Loading from "./Loading"
+
+function RoomContainer({ context }) {
+    const { loading, sortedRooms, rooms } = context;
+    if (loading) {
+        return <Loading />
+    }
+    return (
+        <>
+            <RoomsFilter rooms={rooms} />
+            <RoomList rooms={sortedRooms} />
+        </>
+    )
+}
+
+
+export default withRoomConsumer(RoomContainer);
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import RoomList from "./RoomList"
+// import RoomFilter from "./RoomFilter"
+// import { RoomConsumer } from "../context"
+// import Loading from "./Loading"
+// export default function RoomContainer() {
+//     return (
+//         <RoomConsumer>
+//             {
+//                 value => {
+//                     const { loading, sortedRooms, rooms } = value;
+
+//                     if (loading) {
+//                         return <Loading />
+//                     }
+//                     return (
+//                         <div>
+//                             RoomsContainer
+//                             <RoomFilter rooms={rooms} />
+//                             <RoomList rooms={sortedRooms} />
+//                         </div>
+//                     )
+//                 }
+//             }
+//         </RoomConsumer>
+//     )
+// }
---
+++ src/components/Services.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import  Title from "./Title";
+import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from "react-icons/fa";
+export default class Services extends Component {
+    state = {
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"free cocktails",
+                info:"lorem lorem lorem lorem lorem lorem"
+            },
+            {
+                icon:<FaHiking/>,
+                title:"free cocktails",
+                info:"lorem lorem lorem lorem lorem lorem"
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"free cocktails",
+                info:"lorem lorem lorem lorem lorem lorem"
+            },
+            {
+                icon:<FaBeer/>,
+                title:"free cocktails",
+                info:"lorem lorem lorem lorem lorem lorem"
+            }
+        ]
+    }
+    render() {
+        return (
+            <section className="services">
+                <Title title="services"/>
+                <div className="services-center">
+                {this.state.services.map((item,index)=>{
+                    return(
+                      <article key={index} className="service">
+                        <span>{item.icon}</span>
+                        <h6>{item.title}</h6>
+                        <p>{item.info}</p>
+                      </article>  
+                    );
+                })}
+                </div>
+            </section>
+        )
+    }
+}
